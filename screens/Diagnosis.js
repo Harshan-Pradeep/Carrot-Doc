d8ce7eba91ee1eae43c6f3f68c37f7fee7e36538
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2023-04-24 11:10:44 +0530

    Popup screen done.

--- screens/Diagnosis.js
+++ screens/Diagnosis.js
@@ -1,7 +1,41 @@
-import { View, Text, StyleSheet, TouchableOpacity,Image } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Image, Modal } from 'react-native';
+import React, { useState } from 'react';
+import { BlurView } from 'expo-blur';
 
 const Diagnosis = () => {
+  //useState hook for identify is popup screen visible or ot
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  //set popup screen 
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  }
+
+  //popup screen
+  const Popup = () => {
+    return (
+      <Modal visible={isPopupVisible} transparent={true}>
+        <View style={styles.popupContainer}>
+          <BlurView style={styles.popupBackground} intensity={100} tint="dark" />
+          <View style={styles.popupContent}>
+            <Text style={styles.popupText}>This is the popup!</Text>
+
+            <View style={styles.popupScreenButtons}>
+              <TouchableOpacity style={styles.closeButton} onPress={togglePopup}>
+                <Text style={styles.gotItButton}>Got it</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={togglePopup}>
+                <Text style={styles.cancelButton}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+
+// return other components of the page
   return (
     //main container
     <View style={styles.mainContainer}>
@@ -32,10 +66,14 @@ const Diagnosis = () => {
 
       { /*precaution button*/ }
       <View>
-        <TouchableOpacity style={styles.precautionButton}><Text style={styles.precautionButtonText}>Precautions</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.precautionButton} onPress={togglePopup}><Text style={styles.precautionButtonText}>Precautions</Text></TouchableOpacity>
       </View>
 
+      { /*calling popup screen*/ }
+      <Popup />
+
     </View>
+   
   )
 }
 
@@ -56,8 +94,6 @@ const styles=StyleSheet.create({
         justifyContent:'center',
         alignItems:'center',
         columnGap:180,
-        
-       
     },
 
     //image view section
@@ -138,6 +174,70 @@ const styles=StyleSheet.create({
         textAlign:'center',
       },
 
+      //popup screen styles
+
+      popupContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      popupBackground: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      popupContent: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        padding: 16,
+        alignItems: 'center',
+        width:323,
+        height:669,
+      },
+      popupText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'black',
+        marginBottom: 16
+      },
+      closeButton: {
+        backgroundColor: '#A13600',
+        borderRadius: 8,
+        width:127,
+        height:48,
+        textAlign:'center',
+        justifyContent:'center',
+        
+      },
+      gotItButton: {
+        fontSize: 16,
+        fontWeight:400,
+        color: 'white',
+        textAlign:'center',
+        lineHeight:18,
+        
+        
+      },
+      cancelButton: {
+        fontSize: 16,
+        fontWeight:400,
+        textAlign:'center',
+        lineHeight:18,
+        
+        
+      },
+      popupScreenButtons:{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        flexDirection:'row',
+        gap:80
+
+      }
 
 
 
